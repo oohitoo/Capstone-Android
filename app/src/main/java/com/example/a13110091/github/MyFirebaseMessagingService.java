b145e06a9e8c26a8cea89fff40df57d5d1d057f7
oohitoo <33450618+oohitoo@users.noreply.github.com>
2018-10-02 13:52:04 +0000

    Update MyFirebaseMessagingService.java

--- app/src/main/java/com/example/a13110091/github/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/a13110091/github/MyFirebaseMessagingService.java
@@ -28,7 +28,7 @@ public class MyFirebaseMessagingService extends com.google.firebase.messaging.Fi
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
-                PendingIntent.FLAG_ONE_SHOT);
+                PendingIntent.FLAG_ONE_SHOT); /* intent,PendingIntent.FLAG_UPDATE_CURRENT 출처: http://twinw.tistory.com/50 [흰고래의꿈] */
 
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
